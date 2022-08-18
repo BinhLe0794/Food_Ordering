8feaeace0d34a5a81745161e55b0723b77c32d24
Binh Le <huybinh0794@gmail.com>
2022-08-18 22:49:01 +0700

    Initial Commit

---
+++ Food_Ordering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food_Ordering
+//
+//  Created by Huy Binh on 18/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
